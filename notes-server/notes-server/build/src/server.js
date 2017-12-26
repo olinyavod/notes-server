daa13b081c01619cfeca6c7f30a800b1e7422fd8
olinyavod <olinyavod@gmail.com>
2017-12-26 20:55:57 +1000

    reorganize repository

--- notes-server/notes-server/build/src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var port = 8000;
-var app = express();
-app.listen(port, function () {
-    console.log("Server listen port: " + port);
-});
--- notes-server/notes-server/src/server.ts
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-
-const port = 8000;
-const app = express();
-
-app.listen(port,
-    () => {
-        console.log("Server listen port: " + port);
-    });
